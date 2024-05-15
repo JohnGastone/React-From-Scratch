965fef5b9963eff100837ff090c0bba5552863ce
JohnGastone <johngastone11@gmail.com>
2024-05-15 13:43:18 +0300

    +++

--- renderingList.js
+++ renderingList.js
@@ -1,16 +1,25 @@
 const products = [
-    { name: 'Carbage', id: 1 },
-    { name: 'Coffee', id: 2 },
-    { name: 'Tea', id: 3 },
-    { name: 'Milk', id: 4 },
-    { name: 'Bread', id: 5 },
-    { name: 'Butter', id: 6 },
-    { name: 'Cheese', id: 7 },
+    { name: 'Carbage', isDrink: false, id: 1 },
+    { name: 'Coffee', isDrink: true, id: 2 },
+    { name: 'Tea', isDrink: true, id: 3 },
+    { name: 'Milk', isDrink: true, id: 4 },
+    { name: 'Bread', isDrink: false, id: 5 },
+    { name: 'Butter', isDrink:false, id: 6 },
+    { name: 'Cheese', isDrink: false, id: 7 },
 ];
 
 
-const listItems = products.map(product => 
-    <li key={product.id}>
-        {product.name}
-    </li>
-)
+export default function shoppingList() {
+    const listItems = products.map(product =>
+        <li key={product.id}>
+            {product.name}
+            {product.id}
+        </li>
+    );
+    
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    )
+}
