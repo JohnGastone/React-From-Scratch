3f8413dee8ffa98d7c7b344362354d044ce38f83
JohnGastone <johngastone11@gmail.com>
2024-05-16 11:00:38 +0300

    updating events

--- Day 1/eventsData.js
+++ Day 1/eventsData.js
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 
 export default function MyApp() { 
-    return ( 
+    return ( <div>
+        <h1>
+            Counters that update separately!
+        </h1>
+        <MyButton />
+        <MyAppButton />
+    </div>
+    );
+}
+ 
+function MyButton() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
         <div>
-            <h1>
-                Counters that update separately!
-            </h1>
-            <myButton />
-        </div>);
-    )
- }
+            <button onClick={handleClick}>
+               Clicked {count} times
+            </button>
+        </div>
+    );
+}
