afc7a179aebadb277b038069c798c013737d5cfa
JohnGastone <johngastone11@gmail.com>
2024-05-16 10:53:40 +0300

    ++

---
+++ Day 1/eventsData.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export default function MyApp() { 
+    return ( 
+        <div>
+            <h1>
+                Counters that update separately!
+            </h1>
+            <myButton />
+        </div>);
+    )
+ }
